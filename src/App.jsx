5bab80eb40985cb74cb996621d1e0de18f3eba91
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-01-29 18:47:39 -0300

    se creo la interfaz del formulario de busqueda por categoria

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import Titulo from "./components/Titulo";
+import FormularioBusqueda from "./components/FormularioBusqueda";
 
 function App() {
   return (
     <Container className="my-5">
       <Titulo></Titulo>
+      <FormularioBusqueda></FormularioBusqueda>
     </Container>
   );
 }
--- src/components/FormularioBusqueda.jsx
+++ src/components/FormularioBusqueda.jsx
@@ -1,9 +1,23 @@
+import { Form, Row, Col } from "react-bootstrap";
+
 const FormularioBusqueda = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section className="my-4">
+      <Row>
+        <Col md="4">
+          <p className="lead text-center">Busca por Categoria: </p>
+        </Col>
+        <Col md="8">
+          <Form.Select aria-label="Seleccione una categoria">
+            <option>Seleccione una opción: </option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default FormularioBusqueda;
